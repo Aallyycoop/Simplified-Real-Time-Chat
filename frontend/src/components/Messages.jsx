5687e887e4e0872df9faafc1c77c7435a98dc742
Alexandra Dmitrieva <aallycoop@gmail.com>
2023-07-15 00:25:10 +0300

    step 4: upd te components: Messages and MessageForm

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -12,19 +12,21 @@ const Messages = () => {
 
   return (
     <Col className="p-0 h-100">
-      <div className="bg-light mb-4 p-3 shadow-sm small">
-        <p className="m-0">
-          <b>{`# ${currentChannelName}`}</b>
-        </p>
-        <span className="text-muted">{`${messages.length} сообщений`}</span>
-      </div>
-      <div id="messages-box" className="chat-messages overflow-auto px-5 ">
-        {messages.map((message) => (
-          <div key={message} className="text-break mb-2">{message}</div>
-        ))}
-      </div>
-      <div className="mt-auto px-5 py-3">
-        <MessageForm />
+      <div className="d-flex flex-column h-100">
+        <div className="bg-light mb-4 p-3 shadow-sm small">
+          <p className="m-0">
+            <b>{`# ${currentChannelName}`}</b>
+          </p>
+          <span className="text-muted">{`${messages.length} сообщений`}</span>
+        </div>
+        <div id="messages-box" className="chat-messages overflow-auto px-5 ">
+          {messages.map((message) => (
+            <div key={message} className="text-break mb-2">{message}</div>
+          ))}
+        </div>
+        <div className="mt-auto px-5 py-3">
+          <MessageForm />
+        </div>
       </div>
     </Col>
   );
