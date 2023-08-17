28d967e09dbc5f00e4726e6a853a211b3412821d
Alexandra Dmitrieva <aallycoop@gmail.com>
2023-08-17 23:56:03 +0300

    refactoring: extract AuthProvider.jsx

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import {
   BrowserRouter, Routes, Route, Navigate, useLocation, Link,
 } from 'react-router-dom';
@@ -9,35 +8,9 @@ import ChatPage from './ChatPage';
 import LoginPage from './LoginPage';
 import NotFoundPage from './NotFoundPage';
 import SignUpPage from './SignUpPage';
-import { AuthContext } from '../contexts/index.jsx';
+import AuthProvider from '../contexts/AuthProvider';
 import { useAuth } from '../hooks/index.jsx';
 import routes from '../routes';
-
-const AuthProvider = ({ children }) => {
-  const user = JSON.parse(localStorage.getItem('userId'));
-
-  const [loggedIn, setLoggedIn] = useState(user && user.token);
-
-  const logIn = () => setLoggedIn(true);
-  const logOut = () => {
-    localStorage.removeItem('userId');
-    setLoggedIn(false);
-  };
-
-  const getToken = () => {
-    if (loggedIn) {
-      return user.token;
-    }
-    return null;
-  };
-
-  return (
-    /* eslint-disable-next-line */
-    <AuthContext.Provider value={{ loggedIn, logIn, logOut, getToken, user }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
 
 const PrivateRoute = ({ children }) => {
   const auth = useAuth();
---
+++ frontend/src/contexts/AuthProvider.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { AuthContext } from './index.jsx';
+
+const AuthProvider = ({ children }) => {
+  const user = JSON.parse(localStorage.getItem('userId'));
+
+  const [loggedIn, setLoggedIn] = useState(user && user.token);
+
+  const logIn = () => setLoggedIn(true);
+  const logOut = () => {
+    localStorage.removeItem('userId');
+    setLoggedIn(false);
+  };
+
+  const getToken = () => {
+    if (loggedIn) {
+      return user.token;
+    }
+    return null;
+  };
+
+  return (
+    /* eslint-disable-next-line */
+    <AuthContext.Provider value={{ loggedIn, logIn, logOut, getToken, user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
